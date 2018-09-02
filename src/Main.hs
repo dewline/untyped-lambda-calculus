8ddb2991e11ee87210ff8abf997d107254a0e8d0
dewline <noinline@gmail.com>
2018-09-02 11:18:23 -0400

    [COMPLETE] dramatically improve error reporting

--- src/Main.hs
+++ src/Main.hs
@@ -2,10 +2,15 @@
 -- what's missing from the current parser?
 -- - numeric types: 0, succ t, pred t, iszero t
 -- - parens and paren-matching
--- - error on numeric mismatch
--- - overflow info.. i.e. "true false" should error
--- - underflow info .. i.e. "if true then false" should error
--- - providing file, line and column info for each error
+-- parse time errors:
+--   DONE (kinda) overflow info.. i.e. "true false" should error
+--     - responsibility of caller, not parser
+--   DONE underflow info .. i.e. "if true then false" should error
+--   DONE (kinda) providing file, line and column for each error
+--     - i've set up the framework for this, but not actual code
+-- eval time errors:
+--  - error on numeric mismatch
+--  - underflowing Zero should also error; "prev 0" is not valid
 
 module Main where
 
@@ -14,35 +19,109 @@ main = do
   putStrLn "hello world"
   putStrLn $ "words are: " ++ testexpr
   putStr "result is: "
-  print (utpartparse $ words testexpr)
+  print (utparse (words testexpr) Nothing)
   putStrLn $ "words are: " ++ testexpr2
   putStr "result is: "
-  print (utpartparse $ words testexpr2)
+  print (utparse (words testexpr2) Nothing)
 
 testexpr :: String
 testexpr = "if true then false else true"
-testexpr2 = "if if false then false else false then false else if true then true else false"
+testexpr2 = "if if false then false else false \
+             \ then false else if true then true else false"
 
-utpartparse :: [String] -> TermParsable a
-utpartparse ("true":xs) = TP True' xs
-utpartparse ("false":xs) = TP False' xs
-utpartparse ("then":xs) = utpartparse xs
-utpartparse ("else":xs) = utpartparse xs
-utpartparse ("if":xs) = let (TP t2 xs2) = utpartparse xs
-                            (TP t3 xs3) = utpartparse xs2
-                            (TP t4 xs4) = utpartparse xs3
-                        in TP (Cond t2 t3 t4) xs4
-utpartparse (x:xs) = error $ "term not found: " ++ x
+-------------------
+-- PARSING --------
+-------------------
+-- 1. we don't have to worry about lexing just yet since 'words'
+--    just does the right thing for us. (splits into tokens)
+-- 2. at parse time, i'd want to directly call something like: 
+--        Term t = parse $ words input
+--    and get a Term tree from this. but we cant directly do this 
+--    since we need to keep track of the next tokens to read.
+--    this means that we need to hold both a Term and the string
+--    array that is yet to be parsed.
+-- 3. We'd also want to report parse-time errors to the end user
+--      this is doable by holding the next expected token
+--      (like "else", "then") as part of the datatype.
+--      Then if we find that a completion is not available we can
+--      error out where appropriate
+-- 4. The problem with doing this is that it is somewhat inelegant
+--      Also we wouldn't be able to point to the exact location
+--      where the issue is occurring.
+--      Instead, we can just store the Term tree parsed, and check
+--      on return of the parse function is the returned term
+--      matches the type of the expected term.
+-- 5. What about error handling?
+--      - We can throw exceptions at parse-time...
+--      - Alternatively we can return a Maybe ParsedTerm
+--        the caller can check this and decide whether to proceed.
+--      - But if we return Nothing that wouldn't help
+--      - We can parse "Maybe (Term *)".. then we'd return a PT
+--        that contains Nothing :: Term and nontrivial remainder.
+--        callers can then throw their own error pointing to the
+--        location of the error.
+--        - but this loses the cause of the error right?
+--          we need an ParseError type I guess
+--      - I have a ParseError type that now encodes all parse-time
+--        errors...not using String indexes since that feels hacky.
+--        it feels like a SoC violation...the ParseError should
+--        have all the context needed to report the problem, and
+--        should not rely being an index to an external [] String.
 
-data TermParsable a = TP (Term a) [String] deriving Show
+data ParseError = MissingThenExpr ([] String)
+                | MissingElseExpr ([] String)
+                | UnexpectedToken ([] String)
+                | UnmatchableToken ([] String) String
+                | UnexpectedEndOfInput
+                deriving (Eq, Show)
 
-getTerm :: TermParsable a -> Term a
-getTerm (TP t s) = t
+data ParsedTerm a = PT { term :: Term a
+                       , remainder :: [] String
+                       } deriving Show
 
--- adding 0 term
+type ParseResult a = Either ParseError (ParsedTerm a)
+
+-- utparse takes a string array as input, along with an 'expected'
+-- parse token.
+utparse :: [] String -> Maybe String -> ParseResult a
+utparse ("true" :xs) Nothing       = Right $ PT True' xs
+utparse ("false":xs) Nothing       = Right $ PT False' xs
+utparse ("if"   :xs) Nothing       = utparseif xs
+utparse ("then" :xs) (Just "then") = utparse xs Nothing
+utparse ("else" :xs) (Just "else") = utparse xs Nothing
+utparse x  (Just "then") = Left $ MissingThenExpr x
+utparse x  (Just "else") = Left $ MissingElseExpr x
+utparse [] _             = Left $ UnexpectedEndOfInput
+utparse x  (Just y)      = Left $ UnmatchableToken x y
+utparse x  Nothing       = Left $ UnexpectedToken x
+
+-- factoring out the "if" statement matching since it is getting
+-- quite complicated.
+-- we need to pass errors through, while also matching and 
+-- accumulating "then" and "else" into a Cond.
+utparseif :: [] String -> ParseResult a
+utparseif x = do
+  (PT ifTerm   ifStr)   <- utparse x       Nothing
+  (PT thenTerm thenStr) <- utparse ifStr   (Just "then")
+  (PT elseTerm elseStr) <- utparse thenStr (Just "else")
+  pure $ PT (Cond ifTerm thenTerm elseTerm) elseStr
+
+-- 'Term' is basically ASTNode
+-- We parse into a tree of Term s, but we don't check semantic
+-- correctness until evaluation. Therefore we could end up with
+-- nonsensical Term subtrees like (Cond (Succ 0) True False) or
+-- (Succ True).
+-- adding 0 value and succ, pred, iszero to represent arithexprs
+-- what's a good way really to express termination states?
+--  - i wanted to have a datatype for values that are the
+--    leaves of the Term tree, but not sure how to do this really.
 data Term a = Cond (Term a) (Term a) (Term a)
+            | Succ (Term a)
+            | Pred (Term a)
+            | IsZero (Term a)
             | True'
             | False'
+            | Zero'
             deriving (Eq, Show)
 
 -- evaluation
